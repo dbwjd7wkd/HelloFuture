e1a6fa12719ee3c6123b5db8191a5d1f1291f3b4
YuJeong <dbwjd7wkd@naver.com>
2021-09-13 01:42:16 +0900

    은행 대기표 뽑는 기계 ui 만들기 및 번호증가 상호작용

---
+++ Source/HelloFuture/Private/YJ_GameModeBase.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "YJ_GameModeBase.h"
+
+void AYJ_GameModeBase::AddWaitingNumber()
+{
+	waitingNumber++;
+}
+
+void AYJ_GameModeBase::MinusWaitingNumber()
+{
+	waitingNumber--;
+}
---
+++ Source/HelloFuture/Public/YJ_GameModeBase.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "YJ_GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELLOFUTURE_API AYJ_GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Numbering System")
+		int32 waitingNumber = 0;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Numbering System")
+		int32 order = 0;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void AddWaitingNumber();
+	UFUNCTION(BlueprintCallable)
+	void MinusWaitingNumber();
+};
