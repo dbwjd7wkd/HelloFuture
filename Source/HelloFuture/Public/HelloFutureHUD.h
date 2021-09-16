84f5da666cd624a857d25371874ce3729c4f04e5
Minsu Kim <80089810+keemminxu@users.noreply.github.com>
2021-09-16 17:12:33 +0900

    20210916

---
+++ Source/HelloFuture/Public/HelloFutureHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "HelloFutureHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELLOFUTURE_API AHelloFutureHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
