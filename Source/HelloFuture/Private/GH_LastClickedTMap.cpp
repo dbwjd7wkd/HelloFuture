ce90805dcf0322dd1e0232cd057912f6276559f2
heuristiclife <friud38@naver.com>
2021-10-06 18:17:24 +0900

    1006_1816_GH_TMap_CustomBPProto

---
+++ Source/HelloFuture/Private/GH_LastClickedTMap.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GH_LastClickedTMap.h"
+
+// Sets default values
+AGH_LastClickedTMap::AGH_LastClickedTMap()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	LastClicked.Add("Head");
+	LastClicked.Add("HairStyle");
+	LastClicked.Add("Nose");
+	LastClicked.Add("UpperClothes");
+	LastClicked.Add("UnderClothes");
+	LastClicked.Add("WholeClothes");
+	LastClicked.Add("Glasses");
+	LastClicked.Add("Hat");
+	LastClicked.Add("Bag");
+	LastClicked.Add("Watch");
+
+}
+
+// Called when the game starts or when spawned
+void AGH_LastClickedTMap::BeginPlay()
+{
+	Super::BeginPlay();
+	
+
+}
+
+// Called every frame
+void AGH_LastClickedTMap::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+FString AGH_LastClickedTMap::GetLastClickedItem(FString key)
+{
+// key가 FString이 아니면???
+	// 들어온 key가 유효한지 확인 -> TMap에 존재하는지 확인, FString이 
+	FString value;
+	FString* isKey = LastClicked.Find(key);
+	if (isKey != nullptr)
+	{
+	// 유효하면 값을 돌려줌
+		value = LastClicked[key];
+	}
+	// 유효하지 않으면 "Null"을 돌려줌
+	value = "";
+
+	return value;
+}
+
+void AGH_LastClickedTMap::SetLastClickedItem(FString key, FString value)
+{
+	// 자동으로 오버로딩됨
+	LastClicked.Add(key, value);
+}
+
---
+++ Source/HelloFuture/Public/GH_LastClickedTMap.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GH_LastClickedTMap.generated.h"
+
+UCLASS()
+class HELLOFUTURE_API AGH_LastClickedTMap : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGH_LastClickedTMap();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = LastClicked)
+		TMap<FString, FString> LastClicked;
+
+	UFUNCTION(BlueprintCallable, Category = LastCliked)
+	FString GetLastClickedItem(FString key);
+
+	UFUNCTION(BlueprintCallable, Category = LastCliked)
+	void SetLastClickedItem(FString key, FString value);
+
+};
