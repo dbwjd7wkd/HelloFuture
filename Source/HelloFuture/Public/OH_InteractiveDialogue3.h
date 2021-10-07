3247ded3df31d32141f42f62056df36ee68a7fba
OhSSang95 <skdk1206@naver.com>
2021-10-07 12:11:12 +0900

    2021.10.07 oh1

---
+++ Source/HelloFuture/Public/OH_InteractiveDialogue3.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OH_InteractiveBase.h"
+#include "OH_InteractiveDialogue3.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELLOFUTURE_API AOH_InteractiveDialogue3 : public AOH_InteractiveBase
+{
+	GENERATED_BODY()
+	
+public:
+
+	void Interact() override;
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Dialogue)
+		class UDataTable* Dialogue;
+
+	UFUNCTION(BlueprintCallable, Category = Event)
+		void OnDialogueCompleted();
+
+	bool bIsInteracting = false;
+
+
+
+
+};
