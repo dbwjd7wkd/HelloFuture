66ad31b591825348fc3b5d0fb07ee449636fbbce
Minsu Kim <cuzziman@gmail.com>
2021-10-13 00:36:56 +0900

    21.10.13

--- Source/HelloFuture/Public/YJ_InventoryComponent.h
+++ Source/HelloFuture/Public/YJ_InventoryComponent.h
@@ -26,7 +26,8 @@ enum class EItemEnum : uint8
 	Wheat,
 	WheatSeed = 12,
 	RadishSeed,
-	WaitingTicket
+	WaitingTicket,
+	Fertilizer
 };
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
