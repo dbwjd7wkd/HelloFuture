795445ca143e4df3772080c4626636690504474b
OhSSang95 <skdk1206@naver.com>
2021-10-20 17:00:43 +0900

    2021.10.20 oh3

---
+++ Source/HelloFuture/Private/Oh_MovingNPC2.cpp
@@ -0,0 +1,95 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Oh_MovingNPC2.h"
+#include <AIController.h>
+#include <NavigationSystem.h>
+#include <GameFramework/CharacterMovementComponent.h>
+#include <GameFramework/Character.h>
+#include "HelloFutureCharacter.h"
+#include <Kismet/GameplayStatics.h>
+#include <Engine/EngineTypes.h>
+
+
+// Sets default values
+AOh_MovingNPC2::AOh_MovingNPC2()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AOh_MovingNPC2::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	ai = Cast<AAIController>(GetController());
+
+	ns = UNavigationSystemV1::GetNavigationSystem(GetWorld());
+
+	m_state2 = ENPCState2::Idle;
+
+
+}
+
+// Called every frame
+void AOh_MovingNPC2::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+
+	switch (m_state2)
+	{
+	case ENPCState2::Idle:
+		IdleState();
+		break;
+	case ENPCState2::Walk:
+		WalkState();
+		break;
+	}
+
+
+
+}
+
+// Called to bind functionality to input
+void AOh_MovingNPC2::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AOh_MovingNPC2::IdleState()
+{
+	currnetTime += GetWorld()->DeltaTimeSeconds;
+
+	if (currnetTime > idleDelayTime)
+	{
+		m_state2 = ENPCState2::Walk;
+
+		currnetTime = 0;
+
+		randomPos = UNavigationSystemV1::GetRandomReachablePointInRadius(GetWorld(), GetActorLocation(), 1000);
+	}
+
+}
+
+	void AOh_MovingNPC2::WalkState()
+	{
+		if (!ai) return;
+		GetCharacterMovement()->MaxWalkSpeed = 200;
+
+		//EPathFollowingRequestResult::Type r = ai->MoveToLocation(randomPos);
+
+		// 랜덤 위치로 가자 
+		if (ai->MoveToLocation(randomPos) == EPathFollowingRequestResult::AlreadyAtGoal)
+		{
+			// 이동 가능한 랜덤위치 가져오기
+			//randomPos = UNavigationSystemV1::GetRandomReachablePointInRadius(GetWorld(), GetActorLocation(), 1500);
+
+			m_state2 = ENPCState2::Idle;
+
+			currnetTime = 0;
+		}
+	}
---
+++ Source/HelloFuture/Public/Oh_MovingNPC2.h
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Oh_MovingNPC2.generated.h"
+
+
+UENUM(BlueprintType)
+enum class ENPCState2 : uint8
+{
+	Idle,
+	Walk
+};
+
+
+UCLASS()
+class HELLOFUTURE_API AOh_MovingNPC2 : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AOh_MovingNPC2();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+	UPROPERTY(EditAnywhere, Category = FSM, BlueprintReadWrite)
+		ENPCState2 m_state2;
+
+	UPROPERTY(EditAnywhere, Category = Time)
+		float currnetTime = 0;
+
+	UPROPERTY(EditAnywhere, Category = FSM)
+		float idleDelayTime = 2;
+
+
+	UPROPERTY()
+		class AAIController* ai;
+
+	UPROPERTY()
+		FVector randomPos;
+
+	UPROPERTY()
+		class UNavigationSystemV1* ns;
+
+
+	UPROPERTY()
+		class AActor* target;
+
+
+private:
+	void IdleState();
+	void WalkState();
+
+
+};
