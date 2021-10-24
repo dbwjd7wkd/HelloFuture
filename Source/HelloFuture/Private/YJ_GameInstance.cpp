3a4b0c43fba0c906de44d6891a833ce6ce810bad
YuJeong <dbwjd7wkd@naver.com>
2021-10-25 03:45:20 +0900

    Revert "1025_0159_GH_ShopMapFinished"
    This reverts commit 8ec5e4e6a44e79160c74ca11f9afd45b858e045f.

--- Source/HelloFuture/Private/YJ_GameInstance.cpp
+++ Source/HelloFuture/Private/YJ_GameInstance.cpp
@@ -27,9 +27,9 @@ UYJ_GameInstance::UYJ_GameInstance()
     int length = 13;
     for (int i=0; i < length; i++)
     {
-        int x = 400;
-        int y = -650 + 80 * i;
-        int z = 25;
+        int x = 100;
+        int y = -300 + 50 * i;
+        int z = 45;
         closetSpawnLocation.Add(FVector(x,y,z));
     }
     
