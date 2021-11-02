8ec6e7f9f2bf525002c1a546eecec9223c981456
Minsu Kim <80089810+keemminxu@users.noreply.github.com>
2021-11-02 17:06:08 +0900

    21.11.02 의자 꾸미기

---
+++ Source/HelloFuture/Private/Minsu_Chair.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Minsu_Chair.h"
+#include <Components/StaticMeshComponent.h>
+#include <Components/BoxComponent.h>
+
+// Sets default values
+AMinsu_Chair::AMinsu_Chair()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	bodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	SetRootComponent(bodyMesh);
+
+	collision = CreateDefaultSubobject<UBoxComponent>(TEXT("Collision"));
+	collision->SetupAttachment(bodyMesh);
+}
+
+// Called when the game starts or when spawned
+void AMinsu_Chair::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMinsu_Chair::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
