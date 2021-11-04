82b4cbe78e42f9e24c37171fc7a6c7ecc7ed4e13
dbwjd7wkd <dbwjd7wkd@naver.com>
2021-11-04 17:54:50 +0900

    서버 세션 작업 (advanced sessions 플러그인 추가)

---
+++ Plugins/AdvancedSessions/AdvancedSessions/Intermediate/Build/Win64/UE4Editor/Inc/AdvancedSessions/AdvancedGameSession.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ADVANCEDSESSIONS_AdvancedGameSession_generated_h
+#error "AdvancedGameSession.generated.h already included, missing '#pragma once' in AdvancedGameSession.h"
+#endif
+#define ADVANCEDSESSIONS_AdvancedGameSession_generated_h
+
+#define HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_SPARSE_DATA
+#define HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_RPC_WRAPPERS
+#define HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_RPC_WRAPPERS_NO_PURE_DECLS
+#define HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAAdvancedGameSession(); \
+	friend struct Z_Construct_UClass_AAdvancedGameSession_Statics; \
+public: \
+	DECLARE_CLASS(AAdvancedGameSession, AGameSession, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/AdvancedSessions"), NO_API) \
+	DECLARE_SERIALIZER(AAdvancedGameSession)
+
+
+#define HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_INCLASS \
+private: \
+	static void StaticRegisterNativesAAdvancedGameSession(); \
+	friend struct Z_Construct_UClass_AAdvancedGameSession_Statics; \
+public: \
+	DECLARE_CLASS(AAdvancedGameSession, AGameSession, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/AdvancedSessions"), NO_API) \
+	DECLARE_SERIALIZER(AAdvancedGameSession)
+
+
+#define HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AAdvancedGameSession(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AAdvancedGameSession) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAdvancedGameSession); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAdvancedGameSession); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAdvancedGameSession(AAdvancedGameSession&&); \
+	NO_API AAdvancedGameSession(const AAdvancedGameSession&); \
+public:
+
+
+#define HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AAdvancedGameSession(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAdvancedGameSession(AAdvancedGameSession&&); \
+	NO_API AAdvancedGameSession(const AAdvancedGameSession&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAdvancedGameSession); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAdvancedGameSession); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AAdvancedGameSession)
+
+
+#define HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_PRIVATE_PROPERTY_OFFSET
+#define HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_23_PROLOG
+#define HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_PRIVATE_PROPERTY_OFFSET \
+	HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_SPARSE_DATA \
+	HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_RPC_WRAPPERS \
+	HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_INCLASS \
+	HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_PRIVATE_PROPERTY_OFFSET \
+	HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_SPARSE_DATA \
+	HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_RPC_WRAPPERS_NO_PURE_DECLS \
+	HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_INCLASS_NO_PURE_DECLS \
+	HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h_26_ENHANCED_CONSTRUCTORS \
+static_assert(false, "Unknown access specifier for GENERATED_BODY() macro in class AdvancedGameSession."); \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ADVANCEDSESSIONS_API UClass* StaticClass<class AAdvancedGameSession>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID HelloFuture_Plugins_AdvancedSessions_AdvancedSessions_Source_AdvancedSessions_Classes_AdvancedGameSession_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
