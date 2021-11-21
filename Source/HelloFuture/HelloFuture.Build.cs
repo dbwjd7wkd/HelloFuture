7c90050b294286336708833dbd8d12f385cca48c
heuristiclife <friud38@naver.com>
2021-11-21 21:15:46 +0900

    1121_2115_GH_PocketMoneySQLConnetionTest

--- Source/HelloFuture/HelloFuture.Build.cs
+++ Source/HelloFuture/HelloFuture.Build.cs
@@ -8,8 +8,8 @@ public class HelloFuture : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "AIModule", "NavigationSystem", "UMG", "Networking", "Sockets", "WebBrowser"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "AIModule", "NavigationSystem", "UMG", "Networking", "Sockets", "WebBrowser", "SlateCore", "ImageWrapper", "RenderCore", "MediaAssets", "DatabaseSupport", "SQLiteCore", "SQLiteSupport" });
         PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "SlateCore", "ImageWrapper", "RenderCore", "MediaAssets" });
+		//PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay",  });
 	}
 }
